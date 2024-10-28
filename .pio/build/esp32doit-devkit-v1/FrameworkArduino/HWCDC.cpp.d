85fc21e93423929308976efe15dfd69ba1c56d45
Jose A Correa Jr <jcorrea.junior@gmail.com>
2024-10-28 09:07:07 -0300

    Alterações realizadas durante a compilação do ambiente plataformIO

---
+++ .pio/build/esp32doit-devkit-v1/FrameworkArduino/HWCDC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/HWCDC.cpp.o: \
+ C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
+ C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
+ C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
