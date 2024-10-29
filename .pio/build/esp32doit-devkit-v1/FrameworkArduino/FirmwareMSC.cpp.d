2867042fcde97dae5a3d93e1446987b4bc6cea68
Jose Antonio Correa Junior <118210947+jcorreajunior@users.noreply.github.com>
2024-10-29 09:09:35 -0300

    Delete .pio/build directory
    reiniciando o projeto

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/jcorr/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
